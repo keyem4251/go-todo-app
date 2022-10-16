45de755535c652d3cb30b75155d971d6652e4595
keyem4251 <kisk.emura@gmail.com>
2022-10-16 10:52:15 +0900

    add item status

--- pkg/domain/model/item.go
+++ pkg/domain/model/item.go
@@ -3,11 +3,21 @@ package model
 type Item struct {
 	Title   string
 	Content string
+	Status Status
 }
 
 func NewItem(title string, content string) (*Item, error) {
 	return &Item{
 		Title:   title,
 		Content: content,
+		Status: Open,
 	}, nil
 }
+
+type Status int
+
+const (
+	Open Status = iota
+	InProgress
+	Done
+)
