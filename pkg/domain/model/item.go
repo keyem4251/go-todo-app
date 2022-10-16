519f40319720facc685a2c4b73973aeb08141ac7
keyem4251 <kisk.emura@gmail.com>
2022-10-16 11:07:44 +0900

    add id field

--- pkg/domain/model/item.go
+++ pkg/domain/model/item.go
@@ -1,13 +1,15 @@
 package model
 
 type Item struct {
+	Id int
 	Title   string
 	Content string
 	Status Status
 }
 
-func NewItem(title string, content string) (*Item, error) {
+func NewItem(id int, title string, content string) (*Item, error) {
 	return &Item{
+		Id: id,
 		Title:   title,
 		Content: content,
 		Status: Open,
