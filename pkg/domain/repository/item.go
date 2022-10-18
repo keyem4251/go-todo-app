1a9b5fc8f444c541e9c6b6add423f82671b26016
keyem4251 <kisk.emura@gmail.com>
2022-10-18 23:12:43 +0900

    separeta cqrs

--- pkg/domain/repository/item.go
+++ pkg/domain/repository/item.go
@@ -2,9 +2,14 @@ package repository
 
 import "github.com/keyem4251/go-todo-app/pkg/domain/model"
 
-type ItemRepository interface {
-	Create(item *model.Item) (*model.Item, error)
+type ItemQueryRepository interface {
 	FindById(id int) (*model.Item, error)
+	// FindAll() (*model.Items, error) 複数返す実装
+}
+
+type ItemCommandRepository interface {
+	Create(item *model.Item) (*model.Item, error)
 	Update(item *model.Item) error
 	Delete(item *model.Item) error
 }
+
--- pkg/infrastructure/item.go
+++ pkg/infrastructure/item.go
@@ -7,48 +7,62 @@ import (
 	"github.com/keyem4251/go-todo-app/pkg/domain/model"
 )
 
-type ItemRepository struct {
+type ItemQueryRepositoryImpl struct {
 	Conn *gorm.DB
 }
 
-func NewItemRepository() repository.ItemRepository {
+func NewItemQueryRepository() repository.ItemQueryRepository {
 	db := GetDB()
 
-	return &ItemRepository {
+	return &ItemQueryRepositoryImpl {
 		Conn: db,
 	}
 }
 
-func (ir *ItemRepository) Create(item *model.Item) (*model.Item, error) {
-	if err := ir.Conn.Create(&item).Error; err != nil {
-		return nil, err
-	}
-
-	return item, nil
-}
-
-func (ir *ItemRepository) FindById(id int) (*model.Item, error) {
+func (iqr *ItemQueryRepositoryImpl) FindById(id int) (*model.Item, error) {
 	item := &model.Item{
 		Id: id,
 	}
 
-	if err := ir.Conn.First(&item).Error; err != nil {
+	if err := iqr.Conn.First(&item).Error; err != nil {
 		return nil, err
 	}
 
 	return item, nil
 }
 
-func (ir *ItemRepository) Update(item *model.Item) error {
-	if err := ir.Conn.Model(&item).Updates(&item).Error; err != nil {
+
+type ItemCommandRepositoryImpl struct {
+	Conn *gorm.DB
+}
+
+func NewItemCommandRepository() repository.ItemCommandRepository {
+	db := GetDB()
+
+	return &ItemCommandRepositoryImpl {
+		Conn: db,
+	}
+}
+
+func (icr *ItemCommandRepositoryImpl) Create(item *model.Item) (*model.Item, error) {
+	if err := icr.Conn.Create(&item).Error; err != nil {
+		return nil, err
+	}
+
+	return item, nil
+}
+
+
+func (icr *ItemCommandRepositoryImpl) Update(item *model.Item) error {
+	if err := icr.Conn.Model(&item).Updates(&item).Error; err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (ir *ItemRepository) Delete(item *model.Item) error {
-	if err := ir.Conn.Delete(&item).Error; err != nil {
+func (icr *ItemCommandRepositoryImpl) Delete(item *model.Item) error {
+	if err := icr.Conn.Delete(&item).Error; err != nil {
 		return err
 	}
 	return nil
