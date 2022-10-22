a171ac34226fbdd59410bc229c422df214353401
keyem4251 <kisk.emura@gmail.com>
2022-10-22 17:11:38 +0900

    set mysql

--- pkg/infrastructure/db.go
+++ pkg/infrastructure/db.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 
 	"gorm.io/gorm"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 )
 
 var db *gorm.DB
 
 func Init() *gorm.DB {
 	fmt.Println("start db...")
-	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	dsn := "root:password@tcp(mysql-container:3306)/db_name?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
