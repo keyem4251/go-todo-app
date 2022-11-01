05eed4ccd1dacff08f1c40e196c983fad64c8039
keyem4251 <kisk.emura@gmail.com>
2022-11-01 20:31:00 +0900

    unuse code

--- cmd/go-todo-app/main.go
+++ cmd/go-todo-app/main.go
@@ -4,20 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/keyem4251/go-todo-app/pkg/domain/model"
 	"github.com/keyem4251/go-todo-app/pkg/infrastructure"
 	"github.com/keyem4251/go-todo-app/pkg/adapter/controller"
 )
 
 func main() {
-	item, _ := model.NewItem(1, "Hello", "World")
-	fmt.Printf("Item model: %s, %s\n", item.Title, item.Content)
-
 	infrastructure.Init()
 	
 	fmt.Println("start server")
 	itemController := controller.ItemController{}
 
 	http.HandleFunc("/", itemController.Get)
+	http.HandleFunc("/crate_item", itemController.Post)
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
